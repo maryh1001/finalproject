25b8d0328ea47bcf20c8496ce04466c75fdef182
CaitlinStuder <studercaitlin98@gmail.com>
2022-10-01 05:44:25 -0400

    testing hard coded user and binding the this keyword to the different methods

--- client/src/components/CreateExercise.js
+++ client/src/components/CreateExercise.js
@@ -6,7 +6,11 @@ export default class CreateExercise extends Component {
     constructor(props) {
         super(props);
 
-        
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     
         this.state = {
             username: '',
@@ -17,7 +21,13 @@ export default class CreateExercise extends Component {
         }
     }
 
-    
+    componentDidMount() {
+
+        this.setState({
+            users: ['hard code'],
+            username: 'hard code'
+        })    
+    }
 
     onChangeUsername(e) {
         this.setState({
@@ -37,7 +47,7 @@ export default class CreateExercise extends Component {
         })
     }
 
-    onChangeDate(date) {
+      onChangeDate(date) {
         this.setState({
           date: date
         })
